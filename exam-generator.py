2c5d544acf4f6de31c90d5586c9505b4605cbac3
Rushil Srivastava <rushu0922@gmail.com>
2017-12-14 23:58:09 -0800

    added some command line arguments

--- exam-generator.py
+++ exam-generator.py
@@ -1,17 +1,17 @@
 import random
+import argparse
 
-def createExam(prefixes, roots, suffixes):
+def createExam(prefixes, roots, suffixes, iterations):
     exam = []
     solution = []
-    i = 25
-    while i > 0:
+    while iterations > 0:
         prefix = random.choice(prefixes).replace("\n", "")
         root = random.choice(roots).replace("\n", "")
         suffix = random.choice(suffixes).replace("\n", "")
         word = str(prefix + root + suffix).replace(" ", "")
         exam.append(word)
         solution.append("{} + {} + {}".format(prefix, root, suffix))
-        i = i - 1
+        iterations = iterations - 1
 
     examFile = open('exams/exam.txt', 'w')
     e = 0
@@ -26,33 +26,45 @@ def createExam(prefixes, roots, suffixes):
         s = s + 1
         solutionFile.write("{}. {}\n".format(s, answers))
 
-prefix = open('prefix.txt', 'r')
-prefixes = [line.lower() for line in prefix]
+if __name__ == "__main__":
+    # parse command line options
+    parser = argparse.ArgumentParser()
+    parser.add_argument("words", help="Number of words to generate.", type=int)
+    parser.add_argument("--hush", help="Prevent output. 0=false. 1=true.", nargs='?', const=0, type=int)
+    args = parser.parse_args()
 
-root = open('root.txt', 'r')
-roots = [line.lower() for line in root]
+    # open datasets
+    prefix = open('prefix.txt', 'r')
+    prefixes = [line.lower() for line in prefix]
 
-suffix = open('suffix.txt', 'r')
-suffixes = [line.lower() for line in suffix]
+    root = open('root.txt', 'r')
+    roots = [line.lower() for line in root]
 
-print("Prefixes: {}".format(len(prefixes)))
-for prefix in prefixes:
-    print("    {}".format(prefix))
+    suffix = open('suffix.txt', 'r')
+    suffixes = [line.lower() for line in suffix]
 
-print("\n\n")
+    # handle datasets
+    if args.hush == 1:
+        createExam(prefixes, roots, suffixes, args.words)
+    else:
+        print("Prefixes: {}".format(len(prefixes)))
+        for prefix in prefixes:
+            print("    {}".format(prefix))
 
-print("Roots: {}".format(len(roots)))
-for root in roots:
-    print("    {}".format(root))
+        print("\n\n")
 
-print("\n\n")
+        print("Roots: {}".format(len(roots)))
+        for root in roots:
+            print("    {}".format(root))
 
-print("Suffixes: {}".format(len(suffixes)))
-for suffix in suffixes:
-    print("    {}".format(suffix))
+        print("\n\n")
 
-print("\n\n---------------------------------------------\n")
+        print("Suffixes: {}".format(len(suffixes)))
+        for suffix in suffixes:
+            print("    {}".format(suffix))
 
-print("Creating Exam...")
-createExam(prefixes, roots, suffixes)
-print("Done! Exam and solutions saved to directory. Good luck!")
+        print("\n\n---------------------------------------------\n")
+
+        print("Creating Exam...")
+        createExam(prefixes, roots, suffixes, args.words)
+        print("Done! Exam and solutions saved to directory. Good luck!")
